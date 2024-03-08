0f07f628073b83c88befab04efe29bd627bef9ba
jsh <jsh@curioud.com>
2024-03-08 20:44:49 +0900

    dddd

--- pages/Project/raporapo.vue
+++ pages/Project/raporapo.vue
@@ -9,11 +9,13 @@
   >
   <div class="wrap-all">
     <section class="sec-1">
-      <h3 class="sub-title fade-out" data-scroll data-scroll-speed="2">Web/Mobile Gamification</h3>
+      <div class="box-title">
+        <h3 class="sub-title fade-out" data-scroll data-scroll-speed="2">Web/Mobile Gamification</h3>
       <h1 class="title fade-out" data-scroll data-scroll-speed="2" data-scroll-opacity="0">
         <span class="line"> 게이미피케이션 제작 플랫폼</span>
         <span class="fill">RAPORAPO</span>
       </h1>
+      </div>
       <div class="box-main" data-scroll>
         <img src="@/assets/images/2.jpg" alt="">
       </div>
@@ -22,7 +24,7 @@
       <div class="d-flex">
         <div class="box-left">
           <h5 class="desc">
-            온, 오프라인 연계형 게이미피케이션 제작 플랫폼<br>
+            QR코드 기반 Gamification 제작 플랫폼<br>
             '라포라포로 만드는 온오프라인 교실'
           </h5>
           <div class="box-category">
@@ -72,19 +74,84 @@
           </div>
           <div class="box-text">
             <p class="category">
-              수행업무
+              수행업무 개요
             </p>
             <hr>
             <p class="text">
               웹은 JAVA spring 기반으로 제작되었으며 모바일은 Vue2 기반, 출시 후에 추가된 라포의회는 Vue3, Nuxt3으로 제작되었습니다.
               메인페이지, 모바일 게임, 문제 제작 툴, 토론 게임 등의 페이지의 마크업을 담당했습니다.
             </p>
+            <p class="caution">
+              * 상세한 내용은 하기에 작성했습니다.
+            </p>
           </div>
         </div>
       </div>
     </section>
     <section class="sec-3">
-
+      <h3 class="sec-title">
+        
+      </h3>
+      <div class="flow flow-1">
+        <div class="box-text">
+          <p class="title-flow">
+            메인페이지
+          </p>
+          <p class="desc">
+            캐주얼 게임의 느낌을 주기 위해 작은 오브젝트 들에 트랜지션을 적용하였습니다.
+            메뉴 이동시에는 해당 영역이 확대 되는 트랜지션을 적용하여 사용자가 이동하는 느낌을 주었습니다.
+            확대 후에도 이미지가 깨지지 않기 위해 백그라운드 이미지를 SVG로 변환하여 사용했었으나.
+            이로 인해 트랜지션이 끊기는 문제가 발생하여 확대 이후 크기에 맞게 이미지로 변경하였습니다.
+          </p>
+        </div>
+        <div class="box-media">
+          <video src="@/assets/videos/111.webm" class="video" muted playsinline></video>
+        </div>
+      </div>
+      <div class="flow flow-2">
+        <div class="box-media">
+          <video src="@/assets/videos/222.webm" class="video" muted playsinline></video>
+        </div>
+        <div class="box-text">
+          <p class="title-flow">문제 제작 툴</p>
+          <p class="desc">
+            사용자가 입력한 텍스트에 대해 마크업을 적용하여 사용자가 입력한 텍스트가 어떻게 보일지 미리보기를 제공하였습니다.
+            아이콘의 경우 마우스 오버 및 클릭시 색상이 변경되기 때문에 fontello를 이용하여 아이콘을 폰트화 하였습니다.
+            셀렉트의 경우 select2라는 js 라이브러리를 사용하여 커스텀 하였습니다.
+          </p>
+        </div>
+      </div>
+      <div class="flow-3">
+        <div class="box-text">
+          <p class="title-flow">모바일 게임</p>
+          <p class="desc">
+            Vue를 사용하여 spa 형태의 모바일 게임으로 제작하였습니다.<br>
+            개임별로 주어진 다양한 데이터 및 스테이트의 변화를 감지하여 실시간으로 화면을 업데이트 하였습니다.<br>
+          </p>
+        </div>
+      </div>
+      <div class="flow flow-4 half">
+        <div class="box-media">
+          <img src="@/assets/images/123.jpg" alt="">
+        </div>
+        <div class="box-text">
+          <p class="title-flow">QR 퀴즈 방탈출</p>
+          <p class="desc">
+            QR 퀴즈 방탈출은 참여자들이 테마에 맞는 문제를 해결하고 방을 탈출하는 게임입니다. 각 참가자는 특정 테마 넘버를 받아오게 되며, 이를 통해 게임이 테마에 맞게 다양한 CSS 변경이 가능하도록 구성되어 있습니다. 각 테마는 고유한 분위기와 디자인을 갖추고 있어 참여자들이 게임을 즐기는 동안 새로운 경험을 할 수 있습니다. 
+          </p>
+        </div>
+      </div>
+      <div class="flow flow-5 half">
+        <div class="box-text">
+          <p class="title-flow">HIT & RUN</p>
+          <p class="desc">
+            'HIT & RUN'은 야구를 주제로 한 문제를 맞춰 카드를 획득하는 게임입니다. 게임은 야구의 특성을 반영하여 공격과 수비로 나누어 진행됩니다. 각 턴마다 공격팀은 Strike(스트라이크), Ball(볼), Out(아웃), Hit(안타), HomeRun(홈런) 등의 상태를 경험할 수 있습니다. 이에 따라 알맞은 영상이 재생되고 화면이 업데이트됩니다. 이를 통해 야구의 재미를 경험할 수 있는 게임입니다.
+          </p>
+        </div>
+        <div class="box-media">
+          <img src="@/assets/images/123.jpg" alt="">
+        </div>
+      </div>
     </section>
   </div>
 </div>
@@ -139,6 +206,11 @@ const initFunction = () =>{
     scroll.value.update();
   });
 
+  
+  gsap.from('.box-title', {ease: "power2.out", y:"50%", duration: 0.7, delay: 1})
+  // gsap.from('.title', {ease: "power2.out", top:"30%", duration: 0.7, delay: 1})
+  gsap.from('.box-main', {ease: "power2.out", y:"10%",  width:"80%", duration: 0.7, delay: 1})
+
   scroll.value.on("scroll", (scrollArgs) => {
     // 모든 fade-out 클래스를 가진 요소에 대해 처리
     document.querySelectorAll('.fade-out').forEach((element) => {
@@ -147,31 +219,61 @@ const initFunction = () =>{
       element.style.opacity = opacity.toString();
     });
   });
-  
-  gsap.from('.sub-title', {opacity: 0, ease: "power2.out", y:"50%", duration: 0.7, delay: 1})
-  gsap.from('.title', {opacity: 0, ease: "power2.out", y:"30%", duration: 0.7, delay: 1})
-  gsap.from('.box-main', {opacity: 0, ease: "power2.out", y:"10%",  width:"80%", duration: 0.7, delay: 1})
 
   const sec2 = document.querySelector('.sec-2');
+  const flow3 = document.querySelector('.flow-3');
   // locomotive스크롤로 .sec-2에 스크롤 트리거를 추가
   scroll.value.on('scroll', (args) => {
-    if (args.scroll.y > sec2.offsetTop - window.innerHeight / 2) {
-      gsap.to('.body', { 
-        backgroundColor: '#fff',
-        '--fontColor': '#000',
-        ease: "linear",
-        duration: 1
-      });
-    } else {
+    if (args.scroll.y < sec2.offsetTop - window.innerHeight / 2) {
       gsap.to('.body', { 
         backgroundColor: `${PAGE_INFO[currentIndex].color}`,
         '--fontColor': '#fff',
         ease: "linear",
-        duration: 1
+        duration: 0.5
+      });
+    }
+    // else if (args.scroll.y >= sec2.offsetTop - window.innerHeight / 2 && args.scroll.y < flow3.offsetTop - window.innerHeight / 2) {
+    //   gsap.to('.body', { 
+    //     backgroundColor: '#fff',
+    //     '--fontColor': '#000',
+    //     ease: "linear",
+    //     duration: 0.5
+    //   });
+    // }
+    else {
+      gsap.to('.body', { 
+        backgroundColor: '#fff',
+        '--fontColor': '#000',
+        ease: "linear",
+        duration: 0.5
       });
     }
   });
+  
+  // video에 도착하면 재생
+  const video = document.querySelectorAll('.video');
+  video.forEach((item) => {
+    scroll.value.on('scroll', (args) => {
+      if (args.scroll.y > item.offsetTop - window.innerHeight / 2) {
+        item.play();
+      } else {
+        item.pause();
+      }
+    });
+  });
+
+  const boxMedia = document.querySelectorAll('.box-media video, .box-media img');
+  // box-media에 스크롤한 양 만큼 scale 1로 가기 현재 화면이 box-media보다 위에있으면 scale을 점점 늘린다 최대 1.6 까지
+  // boxMedia.forEach((item) => {
+  //   scroll.value.on('scroll', (args) => {
+  //     if (args.scroll.y < item.offsetTop) {
+  //       const scale = Math.max(1.6 - (args.scroll.y - item.offsetTop + window.innerHeight) / 1000, 1); // 최소 스케일을 1로 설정합니다.
+  //       item.style.transform = `scale(${scale})`;
+  //     }
+  //   });
+  // });
 }
+
 
 </script>
 <style lang="scss">
@@ -187,33 +289,40 @@ const initFunction = () =>{
 
   .wrap-all{
     visibility: hidden;
-    padding: 0 max(4.375rem, 3.65vw);
+    
+  }
+  section{
+    padding: 0 10vw;
   }
   .sec-1{
+    padding: 0 max(4.375rem, 3.65vw);
     text-align: center;
-    
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     margin-bottom: max(4rem, 12.5vw);
+    .box-title{
+      opacity: 1;
+      transform: translateY(0);
+    }
     .sub-title{
       color: $font-color;
       font-size: max(1.125rem, 1.25vw);
-      opacity: 1;
-      transform: translateY(0);
-      font-family: 'GmarketSans';
+      opacity: 0;
       font-weight: 300;
       margin-bottom: 0.8em;
+      position: relative;
+      top: 0;
     }
     .title{
       color: $font-color;
       font-size: max(3.6rem, 4.5vw);
       opacity: 1;
-      transform: translateY(0);
       margin-bottom: 1em;
       font-weight: 900;
-      font-family: 'GmarketSans';
+      position: relative;
+      top: 0;
       .line{
         color: inherit;
         -webkit-text-stroke-width: 1px;
@@ -249,6 +358,8 @@ const initFunction = () =>{
   }
 
   .sec-2{
+    padding-bottom: max(3rem, 10vw);
+    margin-bottom: max(3rem, 3.9vw);
     .d-flex{
       display: flex;
       gap: 5%;
@@ -259,7 +370,6 @@ const initFunction = () =>{
         color: $font-color;
         font-size: max(2.2rem, 2vw);
         line-height: 1.8;
-        font-family: 'GmarketSans';
         font-weight: 500;
         margin-bottom: 0.65em;
         line-height: 1.3;
@@ -299,7 +409,7 @@ const initFunction = () =>{
           // border에 opacity를 주기 위해 rgba로 색상 지정
           padding: 1em 1.5em;
           border-radius: 60px;
-          transition: background-color 800ms, color 800ms, border 800ms;
+          // transition: background-color 800ms, color 800ms, border 800ms;
           position: relative;
           &::before{
             content: '';
@@ -311,18 +421,10 @@ const initFunction = () =>{
             border-radius: 60px;
             border: 1px solid $font-color;
             z-index: -1;
-            transition: background-color 800ms, color 800ms, border 800ms;
+            // transition: background-color 800ms, color 800ms, border 800ms;
             opacity: 0.15;
           }
         }
-      }
-      .caution{
-        color: $font-color;
-        font-size: 0.75rem;
-        font-weight: 300;
-        margin-top: 3%;
-        font-family: 'GmarketSans';
-        opacity: 0.8;
       }
     }
     .box-right{
@@ -334,7 +436,6 @@ const initFunction = () =>{
           font-size: max(1rem, 0.9375vw);
           font-weight: 500;
           margin-bottom: 0.5em;
-          font-family: 'GmarketSans';
         }
         hr{
           margin: 1.25em 0;
@@ -349,15 +450,102 @@ const initFunction = () =>{
           font-size: 0.9375rem;
           font-weight: 300;
           line-height: 1.8;
-          font-family: 'GmarketSans';
         }
       }
     }
-    
+    .caution{
+      color: $font-color;
+      font-size: 0.75rem;
+      font-weight: 300;
+      margin-top: 3%;
+      opacity: 0.8;
+    }
   }
-}
+  .sec-3{
+    // height: 200vh;
+    .sec-title{
+      color: $font-color;
+      font-size: max(2rem, 1.8vw);
+      font-weight: 500;
+      margin-bottom: 2em;
+    }
+    .flow{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      gap: 5%;
+      margin-bottom: 12rem;
+      .box-text{
+        width: 30%;
+        min-width: 30%;
+      }
+      &.half{
+        gap: 0;
+        align-items: stretch;
+        .box-text{
+          width: 50%;
+          min-width: 50%;
+          background: #fff;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          padding: 4vw;
+          .title-flow{
+            color: #000;
+          }
+          .desc{
+            // 색 반전
+            color: #000;
+          }
+        }
+        .box-media{
+          aspect-ratio: 1;
+          background: #1D1D1D;
+          padding: 2vw;
+          img{
+            width: 50%;
+            margin: 0 auto;
+            display: block;
+            transition: 0.7s;
+          }
+          &:hover{
+            img{
+              transform: scale(1.1);
+            }
+          
+          }
+        }
+      }
+      .box-media{
+        flex: 1;
+        aspect-ratio: 16/9;
+        overflow: hidden;
+        
+        video, img{
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+    }
 
-.sec-3{
-  height: 200vh;
+    .title-flow{
+      color: $font-color;
+      font-size: max(1.5rem, 1.56vw);
+      font-weight: 500;
+      margin-bottom: 1em;
+    }
+    .desc{
+      color: $font-color;
+      font-size: max(0.875rem, 0.8333vw);
+      font-weight: 300;
+      line-height: 1.8;
+    }
+  }
+
+  .flow-3{
+    margin-bottom: 2rem;
+  }
 }
 </style>
